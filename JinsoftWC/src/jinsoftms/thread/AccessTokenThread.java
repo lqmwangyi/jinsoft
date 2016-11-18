563573a45d26f602f92483f703a834cec2b02efb
jinsoft <lqmwangyi@163.com>
2016-11-18 18:11:45 +0800

    线程修改token存取方法

--- JinsoftWC/src/jinsoftms/thread/AccessTokenThread.java
+++ JinsoftWC/src/jinsoftms/thread/AccessTokenThread.java
@@ -2,7 +2,10 @@ package jinsoftms.thread;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
 import jinsoftms.database.wechat.AccessToken;
@@ -12,7 +15,7 @@ import jinsoftms.util.WeChatMainUtil;
 
 
 public class AccessTokenThread implements Runnable {
-
+	private static Properties pros = null;
     public static AccessToken accessToken = null;
     
     public void run() {    
@@ -23,9 +26,11 @@ public class AccessTokenThread implements Runnable {
         			accessToken.setAccess_token(access_token);
         		}else{
 	                accessToken = WeChatMainUtil.getAccessToken(WeChatMain.APPID,WeChatMain.APPSECRET);    
-	                if (accessToken != null) {    
-	                    // 休眠7000秒    
-	                    Thread.sleep((accessToken.getExpires_in() - 200) * 1000);    
+	                if (accessToken != null) {
+	                	boolean isW = writeProperties(accessToken);
+	                	if(isW)
+		                    // 休眠7000秒    
+		                    Thread.sleep((accessToken.getExpires_in() - 200) * 1000);    
 	                } else {    
 	                    // 如果access_token为null，3秒后再获取    
 	                    Thread.sleep(30 * 1000);    
@@ -42,14 +47,58 @@ public class AccessTokenThread implements Runnable {
         }    
     } 
     
+    /**
+     * 更新（或插入）一对properties信息(主键及其键值)
+     * 如果该主键已经存在，更新该主键的值；
+     * 如果该主键不存在，则插件一对键值。
+     * @param keyname 键名
+     * @param keyvalue 键值
+     */
+     public static boolean writeProperties(AccessToken accessToken) {       
+		pros = getConfig(null);
+		if (pros == null){
+			return false;
+		}else{
+			String srcPath=pros.getProperty("srcPath");
+			if (srcPath==null){
+				System.out.println("必须提供src路径,参数名是srcPath");
+				return false;
+			}
+			if (srcPath.equals(".")){
+				srcPath=getCurrentSrcPath();
+				if (srcPath==null){
+					System.out.println("在当前的工作目录下无法找到src目录");
+					return false;
+				}
+			}
+			try{
+				// 调用 Hashtable 的方法 put，使用 getProperty 方法提供并行性。
+				// 强制要求为属性的键和值使用字符串。返回值是 Hashtable 调用 put 的结果。
+				OutputStream fos = new FileOutputStream(srcPath);
+				pros.setProperty("access_token", accessToken.getAccess_token());
+				pros.setProperty("expires_in", accessToken.getExpires_in()+"");
+				
+				// 以适合使用 load 方法加载到 Properties 表中的格式，
+				// 将此 Properties 表中的属性列表（键和元素对）写入输出流
+				pros.store(fos, "Update '" + accessToken.getAccess_token() + "' value");
+				pros.store(fos, "Update '" + accessToken.getExpires_in() + "' value");
+				return true;
+			} catch (IOException e) {
+				System.err.println("属性文件更新错误");
+				return false;
+			}
+		}
+     }
+    
     private static String checkPros(){
-		Properties pros=getConfig(null);
+    	pros = getConfig(null);
 		if (pros==null){
 			return null;
 		}else{
 			String srcPath=pros.getProperty("srcPath");
 			if (srcPath==null){
 				System.out.println("必须提供src路径,参数名是srcPath");
+				return null;
 			}
 			if (srcPath.equals(".")){
 				srcPath=getCurrentSrcPath();
